b97fb6afec69248f93f3e8d66a92ad35ca3b016c
ZSchoen <dev@zschoen.dev>
2021-02-12 14:08:26 +0100

    fixed concurrency bug which caused multiple renames of the same file

--- src/fs.rs
+++ src/fs.rs
@@ -1,10 +1,12 @@
 use colored::*;
 use futures::stream::StreamExt;
 use async_std::{future, fs};
-// use std::collections::HashSet;
+use std::collections::HashSet;
+use std::sync::RwLock;
+use std::rc::Rc;
 
 pub async fn rename(opts: &super::cli::Cli, replacer: &super::replace::Replacer) {
-    // let mut outcome = HashSet::new();
+    let targets = Rc::new(RwLock::new(HashSet::new()));
     fs::read_dir(opts.base_path.clone())
         .await
         .unwrap()
@@ -18,17 +20,21 @@ pub async fn rename(opts: &super::cli::Cli, replacer: &super::replace::Replacer)
             None
         })
         .map(|file| file.unwrap().path())
-        .filter(|file| future::ready(replacer.is_match(file).unwrap_or(false)))
+        .filter(|file| future::ready(!targets.read().unwrap().contains(file) && replacer.is_match(file).unwrap_or(false)))
         .map(|file| future::ready((file.clone(), replacer.replace(&file).unwrap())))
-        .for_each(async move |file_futures| {
-            let (old_file, new_file) = file_futures.await;
-            // outcome.insert(new_file.clone());
+        .for_each(|file_futures| {
+            let targets = targets.clone();
+            async move {
+                let (old_file, new_file) = file_futures.await;
+                
+                targets.write().unwrap().insert(new_file.clone());
 
-            if opts.verbose >= 1 {
-            println!("{} {} {}", old_file.to_str().unwrap().red(), "->".blue(), new_file.to_str().unwrap().green());
-            }
-            if opts.run {
-                fs::rename(old_file, new_file).await.unwrap();
+                if opts.verbose >= 1 {
+                    println!("{} {} {}", old_file.to_str().unwrap().red(), "->".blue(), new_file.to_str().unwrap().green());
+                }
+                if opts.run {
+                    fs::rename(old_file, new_file).await.unwrap();
+                }
             }
         })
         .await;
