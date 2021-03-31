2cd3c87502d46835b9fd034cf52742f10910d8f6
ZSchoen <dev@zschoen.dev>
2021-03-31 21:18:12 +0200

    added benchmark & tests

--- benches/main.rs
+++ benches/main.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 extern crate criterion;
 
 use criterion::{async_executor::AsyncStdExecutor, criterion_group, criterion_main, Criterion};
@@ -14,7 +13,7 @@ async fn collected_99_stream() -> Vec<u16> {
         .select_map(|primary_num| {
             let primary_num = primary_num.clone();
             Box::pin(async move {
-                sleep(Duration::from_millis(42)).await;
+                sleep(Duration::from_millis(21)).await;
                 Some(Box::new(stream::iter(
                     10 * primary_num..=10 * primary_num + 9,
                 )))
@@ -29,7 +28,7 @@ async fn collected_49_stream() -> Vec<u16> {
         .select_map(|primary_num| {
             let primary_num = primary_num.clone();
             Box::pin(async move {
-                sleep(Duration::from_millis(42)).await;
+                sleep(Duration::from_millis(21)).await;
                 if primary_num > 4 {
                     return None;
                 }
@@ -53,7 +52,7 @@ async fn collected_999_stream() -> Vec<u16> {
                         |secondary_num| {
                             let secondary_num = secondary_num.clone();
                             Box::pin(async move {
-                                sleep(Duration::from_millis(42)).await;
+                                sleep(Duration::from_millis(21)).await;
                                 Some(Box::new(stream::iter(
                                     10 * secondary_num..=10 * secondary_num + 9,
                                 )))
@@ -67,11 +66,73 @@ async fn collected_999_stream() -> Vec<u16> {
         .await
 }
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| {
+async fn collected_balanced_999_stream() -> Vec<u16> {
+    stream::iter(1_u16..=9_u16)
+        .select_map(|primary_num| {
+            let primary_num = primary_num.clone();
+            Box::pin(async move {
+                sleep(Duration::from_millis(42 - primary_num as u64)).await;
+                Some(Box::new(
+                    stream::iter(10 * primary_num..=10 * primary_num + 9).select_map(
+                        move |secondary_num| {
+                            let secondary_num = secondary_num.clone();
+                            Box::pin(async move {
+                                sleep(Duration::from_millis(21 + primary_num as u64)).await;
+                                Some(Box::new(stream::iter(
+                                    10 * secondary_num..=10 * secondary_num + 9,
+                                )))
+                            })
+                        },
+                    ),
+                ))
+            })
+        })
+        .collect()
+        .await
+}
+
+async fn collected_unbalanced_999_stream() -> Vec<u16> {
+    stream::iter(1_u16..=9_u16)
+        .select_map(|primary_num| {
+            let primary_num = primary_num.clone();
+            Box::pin(async move {
+                sleep(Duration::from_millis(42 - primary_num as u64)).await;
+                Some(Box::new(
+                    stream::iter(10 * primary_num..=10 * primary_num + 9).select_map(
+                        move |secondary_num| {
+                            let secondary_num = secondary_num.clone();
+                            Box::pin(async move {
+                                sleep(Duration::from_millis(21 - primary_num as u64)).await;
+                                Some(Box::new(stream::iter(
+                                    10 * secondary_num..=10 * secondary_num + 9,
+                                )))
+                            })
+                        },
+                    ),
+                ))
+            })
+        })
+        .collect()
+        .await
+}
+
+pub fn benchmark_select_map(c: &mut Criterion) {
+    c.bench_function("SelectMap 99 Stream", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| collected_99_stream())
+    });
+    c.bench_function("SelectMap 49 Stream", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| collected_49_stream())
+    });
+    c.bench_function("SelectMap 999 Stream", |b| {
         b.to_async(AsyncStdExecutor).iter(|| collected_999_stream())
+    });
+    c.bench_function("SelectMap Balanced 999 Stream", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| collected_balanced_999_stream())
+    });
+    c.bench_function("SelectMap Unbalanced 999 Stream", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| collected_unbalanced_999_stream())
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, benchmark_select_map);
 criterion_main!(benches);
--- src/utils/select_map_test.rs
+++ src/utils/select_map_test.rs
@@ -61,6 +61,56 @@ async fn collected_999_stream() -> Vec<u16> {
         .await
 }
 
+async fn collected_balanced_999_stream() -> Vec<u16> {
+    stream::iter(1_u16..=9_u16)
+        .select_map(|primary_num| {
+            let primary_num = primary_num.clone();
+            Box::pin(async move {
+                sleep(Duration::from_millis(42 - primary_num as u64)).await;
+                Some(Box::new(
+                    stream::iter(10 * primary_num..=10 * primary_num + 9).select_map(
+                        move |secondary_num| {
+                            let secondary_num = secondary_num.clone();
+                            Box::pin(async move {
+                                sleep(Duration::from_millis(21 + primary_num as u64)).await;
+                                Some(Box::new(stream::iter(
+                                    10 * secondary_num..=10 * secondary_num + 9,
+                                )))
+                            })
+                        },
+                    ),
+                ))
+            })
+        })
+        .collect()
+        .await
+}
+
+async fn collected_unbalanced_999_stream() -> Vec<u16> {
+    stream::iter(1_u16..=9_u16)
+        .select_map(|primary_num| {
+            let primary_num = primary_num.clone();
+            Box::pin(async move {
+                sleep(Duration::from_millis(42 - primary_num as u64)).await;
+                Some(Box::new(
+                    stream::iter(10 * primary_num..=10 * primary_num + 9).select_map(
+                        move |secondary_num| {
+                            let secondary_num = secondary_num.clone();
+                            Box::pin(async move {
+                                sleep(Duration::from_millis(21 - primary_num as u64)).await;
+                                Some(Box::new(stream::iter(
+                                    10 * secondary_num..=10 * secondary_num + 9,
+                                )))
+                            })
+                        },
+                    ),
+                ))
+            })
+        })
+        .collect()
+        .await
+}
+
 #[async_std::test]
 async fn stream_1_to_99() {
     let mut streamed = collected_99_stream().await;
@@ -78,6 +128,20 @@ async fn stream_1_to_49() {
 #[async_std::test]
 async fn stream_1_to_999() {
     let mut streamed = collected_999_stream().await;
+    streamed.sort_unstable();
+    assert_eq!(streamed, (1..=999).collect::<Vec<_>>());
+}
+
+#[async_std::test]
+async fn stream_1_to_999_balanced() {
+    let mut streamed = collected_balanced_999_stream().await;
+    streamed.sort_unstable();
+    assert_eq!(streamed, (1..=999).collect::<Vec<_>>());
+}
+
+#[async_std::test]
+async fn stream_1_to_999_unbalanced() {
+    let mut streamed = collected_unbalanced_999_stream().await;
     streamed.sort_unstable();
     assert_eq!(streamed, (1..=999).collect::<Vec<_>>());
 }
